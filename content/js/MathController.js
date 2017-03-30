636c79223773e0e59e6437f09bb354f441bca290
Abdulakibu <reachabdula@gmail.com>
2017-03-30 17:13:10 -0400

    initial commit

---
+++ content/js/MathController.js
@@ -0,0 +1,51 @@
+function MathController (){
+
+var holder = 0;
+this.calculate = function (){
+  holder =0;
+
+this.test= this.count.split(' ');
+//console.log("orginal "+this.test);
+
+for(var i = 0; i < this.test.length; i++){
+  if(this.test.indexOf('*') > -1){
+  holder= parseInt(this.test[this.test.indexOf('*')-1]) * parseInt(this.test[this.test.indexOf('*')+1]);
+  this.test.splice(this.test.indexOf('*')-1,3,holder);
+  //console.log("second "+this.test);
+};
+if(this.test.indexOf('/') > -1){
+  holder= parseInt(this.test[this.test.indexOf('/')-1]) / parseInt(this.test[this.test.indexOf('/')+1]);
+  this.test.splice(this.test.indexOf('/')-1,3,holder);
+  //console.log("third "+this.test);
+};
+
+if(this.test.indexOf('+') > -1){
+  holder= parseInt(this.test[this.test.indexOf('+')-1]) + parseInt(this.test[this.test.indexOf('+')+1]);
+  this.test.splice(this.test.indexOf('+')-1,3,holder);
+  //console.log("forth "+this.test);
+};
+if(this.test.indexOf('-') > -1){
+  holder= parseInt(this.test[this.test.indexOf('-')-1]) - parseInt(this.test[this.test.indexOf('-')+1]);
+  this.test.splice(this.test.indexOf('-')-1,3,holder);
+  //console.log("fifth "+this.test);
+};
+}
+
+
+if(isNaN(this.test)){
+  this.test = "Oops something went wrong. Please reevaluate you entry and try again.";
+} else {
+  this.test = this.test[0];
+}
+}
+
+
+
+};
+
+
+
+
+angular
+.module('app')
+.controller('MathController',MathController);
---
+++ content/js/math.directive.js
@@ -0,0 +1,19 @@
+function counter (){
+  return{
+    scope: {},
+    restrict: 'E',
+    controller: 'MathController as math',
+    template:`<div class='math'>
+    <form>
+    <input type='text' ng-model='math.count' />
+    <button type='submit' ng-click='math.calculate();'>Enter</button>
+    </form>
+    <div>{{math.test}}</div>
+    </div>`
+  };
+
+};
+
+angular
+.module('app')
+.directive('counter',counter);
