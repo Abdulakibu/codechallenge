7a35585171f81e079e8a8dde13403fdd97592047
Abdulakibu <reachabdula@gmail.com>
2017-03-30 17:57:13 -0400

    update

--- content/js/MathController.js
+++ content/js/MathController.js
@@ -8,27 +8,34 @@ this.test= this.count.split(' ');
 //console.log("orginal "+this.test);
 
 for(var i = 0; i < this.test.length; i++){
+  for(var i = 0; i < this.test.length; i++){
   if(this.test.indexOf('*') > -1){
   holder= parseInt(this.test[this.test.indexOf('*')-1]) * parseInt(this.test[this.test.indexOf('*')+1]);
   this.test.splice(this.test.indexOf('*')-1,3,holder);
-  //console.log("second "+this.test);
-};
-if(this.test.indexOf('/') > -1){
+  console.log("second "+this.test);
+  }
+  };
+  for(var i = 0; i < this.test.length; i++){
+    if(this.test.indexOf('/') > -1){
   holder= parseInt(this.test[this.test.indexOf('/')-1]) / parseInt(this.test[this.test.indexOf('/')+1]);
   this.test.splice(this.test.indexOf('/')-1,3,holder);
-  //console.log("third "+this.test);
-};
-
-if(this.test.indexOf('+') > -1){
+  console.log("third "+this.test);
+  }
+  };
+  for(var i = 0; i < this.test.length; i++){
+  if(this.test.indexOf('+') > -1){
   holder= parseInt(this.test[this.test.indexOf('+')-1]) + parseInt(this.test[this.test.indexOf('+')+1]);
   this.test.splice(this.test.indexOf('+')-1,3,holder);
-  //console.log("forth "+this.test);
-};
-if(this.test.indexOf('-') > -1){
+  console.log("forth "+this.test);
+  }
+  };
+  for(var i = 0; i < this.test.length; i++){
+  if(this.test.indexOf('-') > -1){
   holder= parseInt(this.test[this.test.indexOf('-')-1]) - parseInt(this.test[this.test.indexOf('-')+1]);
   this.test.splice(this.test.indexOf('-')-1,3,holder);
-  //console.log("fifth "+this.test);
-};
+  console.log("fifth "+this.test);
+  }
+  };
 }
 
 
