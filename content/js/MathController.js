223e52016210d010a9a0a6e35ce72bbd2e1d7b64
Abdulakibu <reachabdula@gmail.com>
2017-04-12 14:41:46 -0400

    clean up

--- content/js/MathController.js
+++ content/js/MathController.js
@@ -12,7 +12,7 @@ console.log("orginal "+this.test);
 for(var i = 0; i < this.test.length; i++){
   for(var i = 0; i < this.test.length; i++){
     if(this.test.indexOf(signs[0]) > -1){
-      holder= parseInt(this.test[this.test.indexOf(signs[0])-indexed]) * parseInt(this.test[this.test.indexOf(signs[0])+1]);
+      holder= parseInt(this.test[this.test.indexOf(signs[0])-indexed]) * parseInt(this.test[this.test.indexOf(signs[0])+indexed]);
       this.test.splice(this.test.indexOf(signs[0])- indexed,replaced,holder);
       console.log("second "+this.test);
   }
@@ -20,21 +20,21 @@ for(var i = 0; i < this.test.length; i++){
 
   for(var i = 0; i < this.test.length; i++){
     if(this.test.indexOf('/') > -1){
-  holder= parseInt(this.test[this.test.indexOf('/')-1]) / parseInt(this.test[this.test.indexOf('/')+1]);
+  holder= parseInt(this.test[this.test.indexOf('/')-1]) / parseInt(this.test[this.test.indexOf('/')+indexed]);
   this.test.splice(this.test.indexOf('/')-indexed,replaced,holder);
   console.log("third "+this.test);
   }
   };
   for(var i = 0; i < this.test.length; i++){
   if(this.test.indexOf('+') > -1){
-  holder= parseInt(this.test[this.test.indexOf('+')-indexed]) + parseInt(this.test[this.test.indexOf('+')+1]);
+  holder= parseInt(this.test[this.test.indexOf('+')-indexed]) + parseInt(this.test[this.test.indexOf('+')+indexed]);
   this.test.splice(this.test.indexOf('+')-indexed,replaced,holder);
   console.log("forth "+this.test);
   }
   };
   for(var i = 0; i < this.test.length; i++){
   if(this.test.indexOf('-') > -1){
-  holder= parseInt(this.test[this.test.indexOf('-')-indexed]) - parseInt(this.test[this.test.indexOf('-')+1]);
+  holder= parseInt(this.test[this.test.indexOf('-')-indexed]) - parseInt(this.test[this.test.indexOf('-')+indexed]);
   this.test.splice(this.test.indexOf('-')-indexed,replaced,holder);
   console.log("fifth "+this.test);
   }
